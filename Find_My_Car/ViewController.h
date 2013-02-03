4e80ecef60e62217faff46de43d94ec533a43c9b
Nick <nick1811@Nick-MacBook.local>
2013-02-03 21:55:48 +0800

    Initial Commit

---
+++ Find_My_Car/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Find_My_Car
+//
+//  Created by Nick on 13/2/3.
+//  Copyright (c) 2013年 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Find_My_Car/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Find_My_Car
+//
+//  Created by Nick on 13/2/3.
+//  Copyright (c) 2013年 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
